bd02b4f4b08c362dada20e2a8d92b64b350b5652
LucasAlvesDaCosta <lucasalves16.la@gmail.com>
2021-10-27 11:11:28 -0300

    Projeto criado

---
+++ src/main/java/com/lucas/cursoJavaSpring/CursoJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.cursoJavaSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoJavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucas/cursoJavaSpring/CursoJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.cursoJavaSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
