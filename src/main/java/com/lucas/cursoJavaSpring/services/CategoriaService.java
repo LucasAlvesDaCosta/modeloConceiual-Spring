571ab44a845b8acfef0a41f1103a749a0eabfcd0
LucasAlvesDaCosta <lucasalves16.la@gmail.com>
2021-11-08 19:50:29 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/lucas/cursoJavaSpring/services/CategoriaService.java
+++ src/main/java/com/lucas/cursoJavaSpring/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.lucas.cursoJavaSpring.domain.Categoria;
 import com.lucas.cursoJavaSpring.repositories.CategoriaRepository;
+import com.lucas.cursoJavaSpring.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,8 +17,7 @@ public class CategoriaService {
 	
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-	}
-
-
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+		}
 }
